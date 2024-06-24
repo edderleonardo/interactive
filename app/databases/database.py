4ebbb525c4d821fa1fb8444521d0501f5a35f121
Edder Ramírez <edderleonardo@gmail.com>
2024-06-23 21:58:41 -0600

    database for tests

--- app/databases/database.py
+++ app/databases/database.py
@@ -11,14 +11,14 @@ USER = os.getenv('POSTGRES_USER')
 PASSWORD = os.getenv('POSTGRES_PASSWORD')
 HOST = os.getenv('POSTGRES_HOST')
 DB = os.getenv('POSTGRES_DATABASE')
+#
+# SQLALCHEMY_DATABASE_URL = f'postgresql://{USER}:{PASSWORD}@{HOST}/{DB}'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{USER}:{PASSWORD}@{HOST}/{DB}'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
-
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./requests.db"
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
+SQLALCHEMY_DATABASE_URL = "sqlite:///./requests.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
